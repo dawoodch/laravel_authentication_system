54acd6eacf29c617e866adc5e18627551b713686
dawoodch <mdawoodch16@gmail.com>
2019-06-20 08:53:21 +0500

    Adding unix_socket to database

--- config/database.php
+++ config/database.php
@@ -61,6 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'unix_socket' => getenv('UNIX_SOCKET')  // ADD NEW UNIC_SOCKET TO DATABSE
         ],
 
         'pgsql' => [
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test' , function(){
+    return "hello this is the testing project";
+});
